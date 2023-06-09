e3f0432fff7c617654adff0d5bcd7574a8f6075f
Cole Usher <coleausher97@gmail.com>
2023-06-09 09:19:53 -0500

    finished services exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -7,6 +7,7 @@ import com.codeup.codeupspringblog.models.User;
 import com.codeup.codeupspringblog.repositories.PostCategoriesRepository;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
+import com.codeup.codeupspringblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,11 +20,13 @@ public class PostController {
     private final PostRepository postsDao;
     private final UserRepository userDao;
     private final PostCategoriesRepository catDao;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postsDao, UserRepository userDao, PostCategoriesRepository catDao) {
+    public PostController(PostRepository postsDao, UserRepository userDao, PostCategoriesRepository catDao, EmailService emailService) {
         this.postsDao = postsDao;
         this.userDao = userDao;
         this.catDao = catDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
@@ -50,7 +53,19 @@ public class PostController {
         return "/posts/create";
     }
 
+    @PostMapping("/posts/create")
+//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    public String submitNewPost(@ModelAttribute Post post) {
+        User user = userDao.findById(1L).get();
+        post.setUser(user);
+        emailService.prepareAndSend(post, "New Post Created!", post.getBody());
+        postsDao.save(post);
 
+
+
+        return "redirect:/posts";
+
+    }
 
     //EDIT HINT FOR EXERCISE
 //    @GetMapping("/parks/{id}/edit")
@@ -60,6 +75,8 @@ public class PostController {
     //3. return the template for the edit.html
     @GetMapping("/posts/{id}/edit")
     public String showEditPostForm(@PathVariable long id, Model model) {
+        // show categories in form
+        model.addAttribute("categories", catDao.findAll());
         Post post = postsDao.findById(id).get();
         model.addAttribute("post", post);
         return "/posts/edit";
@@ -67,24 +84,6 @@ public class PostController {
     @PostMapping("/posts/{id}/edit")
     public String updatePost(@ModelAttribute Post post, @PathVariable long id) {
         post.setId(id);
-        postsDao.save(post);
-        return "redirect:/posts";
-    }
-
-    @PostMapping("/posts/create")
-//    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    public String submitNewPost(@ModelAttribute Post post) {
-//        Post post = new Post(title, body);
-//
-//        List<PostCategories> categories = new ArrayList<>();
-////
-//        for(long categoryId : categoryIds) {
-//            categories.add(catDao.findById(categoryId).get());
-//        }
-
-        User user = userDao.findById(1L).get();
-        post.setUser(user);
-//        post.setCategories(categories);
         postsDao.save(post);
         return "redirect:/posts";
     }
